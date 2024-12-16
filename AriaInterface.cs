c070bb3e323c8afabf3871b7dacd1b0adef5c191
FlygandeFaran <erik.fura@gmail.com>
2024-12-16 15:32:26 +0100

    Add project files.

---
+++ AriaInterface.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TidsBok
+{
+    public static class AriaInterface
+    {
+        private static SqlConnection connection = null;
+
+
+
+        public static void Connect()
+        {
+            //connection = new SqlConnection("data source = ; initial catalog = ; persist security info = True; user id = ; password = ; MultipleActiveResultSets = True");
+
+            string filename = @"\\ltvastmanland.se\ltv\shares\vradiofy\RADIOFYSIK NYSTART\Ö_Erik\02 Programmering\C# scripting\aria_account_information.txt";
+            string connect = File.ReadAllText(filename);
+            connection = new SqlConnection(connect);
+
+            connection.Open();
+        }
+
+        public static void Disconnect()
+        {
+            connection.Close();
+        }
+
+        public static DataTable Query(string queryString)
+        {
+            DataTable dataTable = new DataTable();
+            try
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter(queryString, connection) { MissingMappingAction = MissingMappingAction.Passthrough, MissingSchemaAction = MissingSchemaAction.Add };
+                adapter.Fill(dataTable);
+                adapter.Dispose();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message, "SQL Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            return dataTable;
+        }
+
+        public static DataTable GetScheduledActivities(string startDate, string endDate, string machine)
+        {
+            Connect();
+            DataTable datatable = AriaInterface.Query(@"SELECT DISTINCT
+                                                            Patient.PatientId,
+                                                            ScheduledActivity.ScheduledStartTime
+                                                        FROM
+                                                            Machine,
+                                                            ScheduledActivity,
+                                                            ResourceActivity,
+                                                            Activity,
+                                                            ActivityInstance,
+                                                            Patient
+                                                        WHERE
+                                                            (Patient.PatientId like '19%' OR
+                                                            Patient.PatientId like '20%') AND
+                                                            ScheduledActivity.PatientSer = Patient.PatientSer AND
+                                                            ScheduledActivity.ScheduledStartTime BETWEEN '" + startDate + @"' AND '" + endDate + @"' AND
+                                                            ScheduledActivity.ScheduledActivitySer = ResourceActivity.ScheduledActivitySer AND
+                                                            ResourceActivity.ResourceSer = Machine.ResourceSer AND
+                                                            Machine.MachineName LIKE '" + machine + @"' AND
+                                                            ActivityInstance.ActivityInstanceSer = ScheduledActivity.ActivityInstanceSer AND
+                                                            Activity.ActivitySer = ActivityInstance.ActivitySer AND
+                                                            Activity.ActivityCode != 'Kortkontroll TB1' AND
+                                                            Activity.ActivityCode != 'Kortkontroll TB2' AND
+                                                            Activity.ActivityCode != 'Paus' AND
+                                                            Activity.ActivityCode != 'RESERVERAT' AND
+                                                            Activity.ActivityCode != 'QA'
+                                                        ORDER BY
+                                                            ScheduledActivity.ScheduledStartTime
+                                                            ");
+            Disconnect();
+            return datatable;
+        }
+        public static DataTable GetScheduledActivitiesAlreadyTreated(string startDate, string endDate)
+        {
+            Connect();
+            DataTable datatable = AriaInterface.Query(@"SELECT DISTINCT
+                                                            Patient.PatientId,
+                                                            ScheduledActivity.ScheduledStartTime
+                                                        FROM
+                                                            Machine,
+                                                            ScheduledActivity,
+                                                            ResourceActivity,
+                                                            Activity,
+                                                            ActivityInstance,
+                                                            Patient
+                                                        WHERE
+                                                            (Patient.PatientId like '19%' OR
+                                                            Patient.PatientId like '20%') AND
+                                                            ScheduledActivity.PatientSer = Patient.PatientSer AND
+                                                            ScheduledActivity.ScheduledStartTime BETWEEN '" + startDate + @"' AND '" + endDate + @"' AND
+                                                            ScheduledActivity.ScheduledActivitySer = ResourceActivity.ScheduledActivitySer AND
+                                                            ResourceActivity.ResourceSer = Machine.ResourceSer AND
+                                                            (Machine.MachineName LIKE 'Strålbehandling 1' OR
+                                                            Machine.MachineName LIKE 'Strålbehandling 2') AND
+                                                            ActivityInstance.ActivityInstanceSer = ScheduledActivity.ActivityInstanceSer AND
+                                                            Activity.ActivitySer = ActivityInstance.ActivitySer AND
+                                                            Activity.ActivityCode != 'Kortkontroll TB1' AND
+                                                            Activity.ActivityCode != 'Kortkontroll TB2' AND
+                                                            Activity.ActivityCode != 'Paus' AND
+                                                            Activity.ActivityCode != 'RESERVERAT' AND
+                                                            Activity.ActivityCode != 'QA'
+                                                        ORDER BY
+                                                            ScheduledActivity.ScheduledStartTime
+                                                            ");
+            Disconnect();
+            return datatable;
+        }
+        public static DataTable GetPatientList(string startDate, string endDate, string machine)
+        {
+            Connect();
+            DataTable datatable = AriaInterface.Query(@"SELECT DISTINCT
+                                                            Patient.PatientId
+                                                        FROM
+                                                            Machine,
+                                                            ScheduledActivity,
+                                                            ResourceActivity,
+                                                            Activity,
+                                                            ActivityInstance,
+                                                            Patient
+                                                        WHERE
+                                                            (Patient.PatientId like '19%' OR
+                                                            Patient.PatientId like '20%') AND
+                                                            ScheduledActivity.PatientSer = Patient.PatientSer AND
+                                                            ScheduledActivity.ScheduledStartTime BETWEEN '" + startDate + @"' AND '" + endDate + @"' AND
+                                                            ScheduledActivity.ScheduledActivitySer = ResourceActivity.ScheduledActivitySer AND
+                                                            ResourceActivity.ResourceSer = Machine.ResourceSer AND
+                                                            Machine.MachineName LIKE '" + machine + @"' AND
+                                                            ActivityInstance.ActivityInstanceSer = ScheduledActivity.ActivityInstanceSer AND
+                                                            Activity.ActivitySer = ActivityInstance.ActivitySer AND
+                                                            Activity.ActivityCode != 'Kortkontroll TB1' AND
+                                                            Activity.ActivityCode != 'Kortkontroll TB2' AND
+                                                            Activity.ActivityCode != 'RESERVERAT' AND
+                                                            Activity.ActivityCode != 'QA'
+                                                        ORDER BY
+                                                            Patient.PatientId
+                                                            ");
+            Disconnect();
+            return datatable;
+        }
+    }
+}
---
+++ Interruption.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace TidsBok
+{
+    partial class Interruption
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnYes = new System.Windows.Forms.Button();
+            this.btnNo = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnYes
+            // 
+            this.btnYes.DialogResult = System.Windows.Forms.DialogResult.Yes;
+            this.btnYes.Location = new System.Drawing.Point(15, 37);
+            this.btnYes.Name = "btnYes";
+            this.btnYes.Size = new System.Drawing.Size(75, 23);
+            this.btnYes.TabIndex = 0;
+            this.btnYes.Text = "Ja";
+            this.btnYes.UseVisualStyleBackColor = true;
+            // 
+            // btnNo
+            // 
+            this.btnNo.DialogResult = System.Windows.Forms.DialogResult.No;
+            this.btnNo.Location = new System.Drawing.Point(139, 37);
+            this.btnNo.Name = "btnNo";
+            this.btnNo.Size = new System.Drawing.Size(75, 23);
+            this.btnNo.TabIndex = 1;
+            this.btnNo.Text = "Nej";
+            this.btnNo.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(202, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Programmet har pausats. Vill du forstätta?";
+            // 
+            // Interruption
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(237, 76);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnNo);
+            this.Controls.Add(this.btnYes);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Interruption";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Avbrott";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnYes;
+        private System.Windows.Forms.Button btnNo;
+        private System.Windows.Forms.Label label1;
+    }
+}
